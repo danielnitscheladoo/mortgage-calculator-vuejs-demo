0a9a7724acbe9863dde436ee239dadf1d6c1ddb4
Daniel Nitsche <daniel@ladoo.com.au>
2018-07-24 14:59:01 +1000

    Basic data for inputs.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <label>
+      Borrow
+      <input
+        type="number"
+        v-model="borrowed"
+      >
+    </label>
+    <label>
+      Interest rate
+      <input
+        type="number"
+        v-model="interestRate"
+      >
+    </label>
+    <label>
+      Length of loan
+      <input
+        type="number"
+        v-model="loanLengthInYears"
+      >
+    </label>
+    <label>
+      Fees
+      <input
+        type="number"
+        v-model="fees"
+      >
+    </label>
+
+    <p class="repayments">Your repayments will be<br> {{ repayments }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+      borrowed: 500000,
+      interestRate: 5.00,
+      repaymentFrequency: 12,
+      loanLengthInYears: 30,
+      fees: 10,
+      feeFrequency: 12
+    }
+  },
+  computed: {
+    repayments() {
+      let monthlyRate = parseFloat(this.interestRate) / 100 / 12;
+      let loanLengthInMonths = this.loanLengthInYears * 12;
+      let repayment = this.borrowed * (monthlyRate * Math.pow(1 + monthlyRate, loanLengthInMonths)) / (Math.pow(1 + monthlyRate, loanLengthInMonths) - 1);
+      return `$${Math.round(repayment + this.fees)} per month`;
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  #app {
+    font-family: "Helvetica Neue", sans-serif;
+    margin: 2rem auto;
+    width: 30rem;
+  }
+
+  label, input {
+    display: block;
+    font-size: 1.25rem;
+  }
+
+  label {
+    margin-bottom: 1rem;
+  }
+
+  input {
+    margin-top: .25rem;
+    padding: .25rem;
+    text-align: right;
+    width: 7rem;
+  }
+
+  .repayments {
+    font-size: 2rem;
+  }
 </style>
